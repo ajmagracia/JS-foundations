252b7ff9e5e62135e9f9e6df1faedb9227a75cc5
AJ Magracia <ajmagracia@gmail.com>
2018-08-09 00:04:16 -0700

    completed more loops practice until prompt questions

--- more-loops-practice.js
+++ more-loops-practice.js
@@ -1,25 +1,62 @@
-
 // Loops/Iteration Practice 2: while/do while
 
 // Below are exercises to get you practicing with while and for in loops. Beneath each prompt write the code to fulfill the exercise requirement.
 
 // Exercise 1
+console.log('Exercise 1')
 // Write a while loop that logs each number from 0 to 10.
+var i = 0
+while (i < 11) {
+  console.log(i)
+  i++
+}
 
 // Exercise 2
+console.log('Exercise 2')
 // Write a while loop that logs each number from 10 to 0.
-
+//i is starting at 11 from previous problem
+while (i > 0) {
+  i--
+  console.log(i)
+}
 // Exercise 3
+console.log('Exercise 3')
 // Write a do while loop that logs each number from 0 to 10
-
+do {
+  console.log(i)
+  i++
+} while (i < 11)
 // Exercise 4
+console.log('Exercise 4')
 // Write a do while loop that logs each number from 10 to 0.
+//i is starting at 11 from previous problem
+do {
+  i--
+  console.log(i)
+} while (i > 0)
 
 // Exercise 5
+console.log('Exercise 5')
 // Write a function that takes a number as an argument and uses a while loop to count up to that number from 0.
+function countWhile(num) {
+  while (i < num + 1) {
+    console.log(i)
+    i++
+  }
+}
+countWhile(10)
 
 // Exercise 6
+console.log('Exercise 6')
 // Write a function that takes a number as an argument and uses a do while loop to count up to that number from 0.
+var count = num => {
+  i = 0
+  do {
+    console.log(i)
+    i++
+  } while (i < num + 1)
+}
+count(10)
 
 // Exercise 7
 // Write a function that uses while loop to prompt a user for an even number until their entry is even.
